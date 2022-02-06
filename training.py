8ceb47b3d3abb6f8e55e0f4e4ac815e74ec357ce
EuriskoDeveloment <re@pandema.com>
2022-02-06 22:19:58 +0100

    Add model training function

--- training.py
+++ training.py
@@ -15,18 +15,27 @@ with open('config.json','r') as f:
 dataset_csv_path = os.path.join(config['output_folder_path']) 
 model_path = os.path.join(config['output_model_path']) 
 
+numeric_predictors = ['lastmonth_activity', 'lastyear_activity', 'number_of_employees']
+target = 'exited'
 
 #################Function for training the model
 def train_model():
     
     #use this logistic regression for training
-    LogisticRegression(C=1.0, class_weight=None, dual=False, fit_intercept=True,
+    lr_model = LogisticRegression(C=1.0, class_weight=None, dual=False, fit_intercept=True,
                     intercept_scaling=1, l1_ratio=None, max_iter=100,
-                    multi_class='warn', n_jobs=None, penalty='l2',
+                    multi_class='auto', n_jobs=None, penalty='l2',
                     random_state=0, solver='liblinear', tol=0.0001, verbose=0,
                     warm_start=False)
-    
-    #fit the logistic regression to your data
-    
-    #write the trained model to your workspace in a file called trainedmodel.pkl
 
+    trainingdata=pd.read_csv(os.path.join(os.getcwd(), dataset_csv_path, 'finaldata.csv'))
+    X=trainingdata.loc[:, numeric_predictors].values.reshape(-1, len(numeric_predictors))
+    y=trainingdata[target].values.reshape(-1, 1).ravel()
+
+    #fit the logistic regression to your data
+    lr_model.fit(X, y)
+    #write the trained model to your workspace in a file called trainedmodel.pkl
+    pickle.dump(lr_model, open(os.path.join(model_path, 'trainedmodel.pkl'), 'wb')) 
+
+if __name__ == '__main__':
+    train_model()
