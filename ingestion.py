67da5527e75648a835b6124f90fb97fde1390749
EuriskoDeveloment <re@pandema.com>
2022-02-06 21:42:22 +0100

    Add data ingestion code

--- ingestion.py
+++ ingestion.py
@@ -1,3 +1,4 @@
+from unittest import skip
 import pandas as pd
 import numpy as np
 import os
@@ -19,6 +20,27 @@ output_folder_path = config['output_folder_path']
 #############Function for data ingestion
 def merge_multiple_dataframe():
     #check for datasets, compile them together, and write to an output file
+    df_list = pd.DataFrame(columns=['corporation', 'lastmonth_activity', 'lastyear_activity', 'number_of_employees', 'exited'])
+    ingested_files = 'The following files have been read:\n'
+    
+    filenames = (x for x in os.listdir(os.path.join(os.getcwd(), input_folder_path)) if x[-4:] == '.csv')
+
+    for filename in filenames:
+        print("reading file " + filename)
+        df1 = pd.read_csv(os.path.join(os.getcwd(), input_folder_path, filename))
+        df_list = df_list.append(df1)
+        ingested_files += (filename + '\n')
+    result=df_list.drop_duplicates()
+
+    output_dir_fullname = os.path.join(os.getcwd(), output_folder_path)
+    if not os.path.exists(output_dir_fullname):
+        os.mkdir(output_dir_fullname)
+    result.to_csv(os.path.join(output_dir_fullname, 'finaldata.csv'), index=False)
+
+    with open(os.path.join(output_dir_fullname, 'ingestedfiles.txt'), 'w') as f:
+        f.write(ingested_files)
+
+    return 0
 
 
 
